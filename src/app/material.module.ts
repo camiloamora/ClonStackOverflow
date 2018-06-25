71a018fd3421685ed4a9a8585abb869194000606
unknown <camilo.mora@vasslatam.com>
2018-06-25 15:03:11 -0500

    Se agrega la libreria de material al proyecto

---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatToolbarModule,
+    MatIconModule
+  } from '@angular/material';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule
+]
+
+@NgModule({
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule{}
