7d91e88b3983a827d355a94628cd7a2ba524e340
unknown <camilo.mora@vasslatam.com>
2018-06-26 11:04:51 -0500

    Se agrega la pantalla de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import 'hammerjs';
 import {QuestionDetailComponent } from './question/question-detail.component';
 import {AsnwerFormComponent } from './answer/answer-form.component';
 import { SigninScreenComponent } from './auth/signin-screen.component';
+import {SignupScreenComponent } from './auth/signup-screen.component';
 import { MomentModule } from 'angular2-moment';
 
 @NgModule({
@@ -18,7 +19,8 @@ import { MomentModule } from 'angular2-moment';
     AppComponent,
     QuestionDetailComponent,
     AsnwerFormComponent,
-    SigninScreenComponent
+    SigninScreenComponent,
+    SignupScreenComponent
   ],
   imports: [
     BrowserModule,
