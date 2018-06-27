083aa6fdd9d71a1bf6979382ce0a4ebe8ba42f75
unknown <camilo.mora@vasslatam.com>
2018-06-27 16:30:49 -0500

    Se crea la ruta /api/questions en el API

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import {question} from './routers'
 
 const app = express()
 
-app.get('/', (req, res) =>
-  res.send('Hola desde express')
-)
+// app.get('/', (req, res) =>
+//   res.send('Hola desde express')
+// )
+
+app.use('/api/questions', question)
 
 export default app
---
+++ server/routers/question.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express.Router()
+
+const question = {
+  _id: 1,
+  title: '¿Como reutilizo un componente en android',
+  description:'Esta es mi pregunta',
+  createdAt: new Date(),
+  icon: 'devicon-android-plain',
+  answers: [],
+  user:{
+    firstName: 'Camilo',
+    lastName: 'Mora',
+    email: 'kalufau@hotmail.com',
+    password:'123456'
+  }
+}
+
+const questions = new Array(10).fill(question)
+
+// /api/questions
+app.get('/', (req, res) => res.status(200).json(questions))
+// /api/questions/id
+app.get('/:id', (req, res) => res.status(200).json(question))
+
+export default app
