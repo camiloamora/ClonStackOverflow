3850735fa2d3f8bd5310b861ca2dc8df6622715c
unknown <camilo.mora@vasslatam.com>
2018-06-28 15:07:12 -0500

    se agrega ruta para crear una respuesta en el Backend

--- server/routers/question.js
+++ server/routers/question.js
@@ -2,6 +2,24 @@ import express from 'express'
 
 const app = express.Router()
 
+const currentUser ={
+  firstName: 'Camilo',
+  lastName: 'Mora',
+  email: 'kalufau@hotmail.com',
+  password:'123456'
+}
+
+function questionMiddleware(req, res, next){
+  const { id } = req.params
+  req.question = questions.find(({ _id }) => _id === +id)
+  next()
+}
+
+function userMiddleware(req, res, next){
+  req.user = currentUser
+  next()
+}
+
 const question = {
   _id: 1,
   title: 'Hola ¿Como reutilizo un componente en android',
@@ -28,27 +46,31 @@ app.get('/', (req, res) =>
 })
 
 // GET /api/questions/id
-app.get('/:id', (req, res) => {
-  console.log('entro por id')
-  const { id } = req.params
-  const q = questions.find(({ _id }) => _id === +id)
-  res.status(200).json(q)
+app.get('/:id', questionMiddleware, (req, res) => {
+  // console.log('entro por id')
+  // const { id } = req.params
+  // const q = questions.find(({ _id }) => _id === +id)
+  res.status(200).json(req.question)
 })
 
 // POST /api/question
-app.post('/', (req,res) =>{
+app.post('/', userMiddleware, (req,res) =>{
   const question = req.body
   question._id = +new Date()
-  question.user =   {
-    email: 'kalufau@hotmail.com',
-    password: '123456',
-    firstName: 'Camilo',
-    lastName: 'Mora'
-  }
+  question.user = req.user
   question.createdAt = new Date()
   question.answers = []
   questions.push(question)
   res.status(201).json(question)
+})
+
+app.post('/:id/answers', questionMiddleware, userMiddleware , (req, res) =>{
+  const answer = req.body
+  const q = req.question
+  answer.createdAt = new Date()
+  answer.user = req.user
+  q.answer.push(answer)
+  res.status(201).json(answer)
 })
 
 export default app
